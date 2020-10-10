18d2a458bb081a587fe5013e06d4334700d4c017
Devin Wilson <craigvin84@yahoo.com>
2020-10-10 17:47:47 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -8,3 +8,15 @@ def my_each(array)
 end
 
 my_each(list) {|i| puts "So I think #{i} is a piece of shit"}
+
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
+
+hello(list) {|i| "Hello #{i} what's up?"}
