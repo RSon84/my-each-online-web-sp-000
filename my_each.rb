198ab7e6bbed50e76d3668679610131a43690186
Devin Wilson <craigvin84@yahoo.com>
2020-10-10 17:35:13 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,6 +2,10 @@ collection = [1,2,3,4]
 
 def my_each(collection) # put argument(s) here
   my_each(collection) do |i|
-    puts i  # code here
+    new_collection = 0
+    while new_collection < collection.length
+      yeild(array[new_collection])
+      new_collection = new_collection + 1# code here
 end
+array
 end
